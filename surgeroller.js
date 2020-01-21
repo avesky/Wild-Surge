4ed796c72b35faaa3631c9ca31004fe876c90ed4
sullage <sullage@pessimistic-it.com>
2020-01-21 00:38:46 -0600

    added comments and extracted

--- surgeroller.js
+++ surgeroller.js
@@ -8,7 +8,6 @@ const surgeArray = [
 
 const surgePick = Math.floor(Math.random() * surgeArray.length);
 
-//const dieToRoll =
 const regexDie = /d([0-9]+)/;
 const regexNumDie = /([0-9]+)d/;
 const regexTotal = /[0-9]+d[0-9]+/;
@@ -31,13 +30,38 @@ for (let i = 0; i < dieToSum.length; i++) {
 }
 // console.log(dieToSum, surgeArray[surgePick].replace(dieReplace, dieTotal));
 
+const surge = () => {
+	// *** constants 
+	const findDiceRegex = /(\d+)d(\d+)/g; // regexr.com/4sk80
 
-const rollADie = (sides) => Math.floor(Math.random() * sides) + 1
-const evaluateDice = (dice, sides) => 
-	Array.from({length: dice}, () => rollADie(sides))
-	.reduce((sum, curr) => sum + curr, 0)
+	// *** helper functions
+	// returns an integer in the interval [0, limit)
+	const randomNumber = (limit) => Math.floor(Math.random() * limit);
 
-// see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Specifying_a_function_as_a_parameter
-const surgeWithDiceReplaced = surgeArray[surgePick]
-	.replace(/(\d+)d(\d+)/g, (_, num, dice) => evaluateDice(num, dice)); 
-console.log(surgeWithDiceReplaced);
+	// returns an integer in the interval [1, sides]
+	// might be replaced by chancejs eventually: https://chancejs.com/
+	const rollADie = (sides) => randomNumber(sides) + 1;
+
+	// returns the sum of an array of numbers
+	const sumArray = (array) => array.reduce((sum, curr) => sum + curr, 0);
+
+	// returns an array of dice rolls
+	const getArrayOfDiceRolls = (numberOfDice, numberOfSides) =>
+		// see "using arrow functions" under examples: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from#Examples
+		Array.from({length: numberOfDice}, () => rollADie(numberOfSides));
+	
+
+	// *** surge logic 
+	const surgeIndex = randomNumber(surgeArray.length);
+	const rawSurgeText = surgeArray[surgeIndex];
+	
+	return rawSurgeText
+		// see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Specifying_a_function_as_a_parameter
+		.replace(findDiceRegex, (_, numberOfDice, numberOfSides) => {
+			const diceRolls = getArrayOfDiceRolls(numberOfDice, numberOfSides);
+			
+			return sumArray(diceRolls);
+		});
+}
+
+console.log(surge());
