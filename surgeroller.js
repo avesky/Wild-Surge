7fe92553866555170f01b3e0ded3651486309edf
Andreas Christianson <sullage@pessimistic-it.com>
2020-01-21 21:54:42 -0600

    added a big list of wild magic

--- surgeroller.js
+++ surgeroller.js
@@ -1,32 +1,27 @@
-const surgeArray = [
-	"0001 1d10 of caster's fingers turn to stone",
-	'0002 3d100 bees swarm harmlessly around the caster for several weeks',
-	"0003 6d100 sparkling motes dance about the caster's head until dawn",
-	"0004 9d100% of caster's body turns to iron for that many rounds",
-	'0005 56d12 ducklings identify the caster as their mother'
-];
+const surgeArray = require('./clean.json');
 
 const surgePick = Math.floor(Math.random() * surgeArray.length);
+// console.log(surgePick);
 
-//const dieToRoll =
-const regexDie = /d([0-9]+)/;
-const regexNumDie = /([0-9]+)d/;
-const regexTotal = /[0-9]+d[0-9]+/;
+// //const dieToRoll =
+// const regexDie = /d([0-9]+)/;
+// const regexNumDie = /([0-9]+)d/;
+// const regexTotal = /[0-9]+d[0-9]+/;
 
-const dieToRoll = regexDie.exec(surgeArray[surgePick]);
-const numDie = regexNumDie.exec(surgeArray[surgePick]);
-const dieReplace = regexTotal.exec(surgeArray[surgePick]);
+// const dieToRoll = regexDie.exec(surgeArray[surgePick]);
+// const numDie = regexNumDie.exec(surgeArray[surgePick]);
+// const dieReplace = regexTotal.exec(surgeArray[surgePick]);
 
-function rollDie() {
-	return Math.floor(Math.random() * dieToRoll[1]) + 1;
-}
-const dieToSum = [];
-for (let i = 0; i < numDie[1]; i++) {
-	dieToSum.push(rollDie());
-}
+// function rollDie() {
+// 	return Math.floor(Math.random() * dieToRoll[1]) + 1;
+// }
+// const dieToSum = [];
+// for (let i = 0; i < numDie[1]; i++) {
+// 	dieToSum.push(rollDie());
+// }
 
-dieTotal = 0;
-for (let i = 0; i < dieToSum.length; i++) {
-	dieTotal += dieToSum[i];
-}
-console.log(dieToSum, surgeArray[surgePick].replace(dieReplace, dieTotal));
+// dieTotal = 0;
+// for (let i = 0; i < dieToSum.length; i++) {
+// 	dieTotal += dieToSum[i];
+// }
+console.log(surgePick, surgeArray[surgePick]);
