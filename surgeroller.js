82527d2802b7fad7b1b9657ddde90866b35f795e
Andreas Christianson <sullage@pessimistic-it.com>
2020-01-21 22:03:08 -0600

    uncommented code

--- surgeroller.js
+++ surgeroller.js
@@ -1,27 +1,28 @@
 const surgeArray = require('./clean.json');
 
 const surgePick = Math.floor(Math.random() * surgeArray.length);
-// console.log(surgePick);
 
-// //const dieToRoll =
-// const regexDie = /d([0-9]+)/;
-// const regexNumDie = /([0-9]+)d/;
-// const regexTotal = /[0-9]+d[0-9]+/;
+//const dieToRoll =
+const regexDie = /d([0-9]+)/;
+const regexNumDie = /([0-9]+)d/;
+const regexTotal = /[0-9]+d[0-9]+/;
 
-// const dieToRoll = regexDie.exec(surgeArray[surgePick]);
-// const numDie = regexNumDie.exec(surgeArray[surgePick]);
-// const dieReplace = regexTotal.exec(surgeArray[surgePick]);
+const dieToRoll = regexDie.exec(surgeArray[surgePick]);
+const numDie = regexNumDie.exec(surgeArray[surgePick]);
+const dieReplace = regexTotal.exec(surgeArray[surgePick]);
 
-// function rollDie() {
-// 	return Math.floor(Math.random() * dieToRoll[1]) + 1;
-// }
-// const dieToSum = [];
-// for (let i = 0; i < numDie[1]; i++) {
-// 	dieToSum.push(rollDie());
-// }
+function rollDie() {
+	return Math.floor(Math.random() * dieToRoll[1]) + 1;
+}
+const dieToSum = [];
+dieTotal = 0;
+if (numDie && dieToRoll) {
+	for (let i = 0; i < numDie[1]; i++) {
+		dieToSum.push(rollDie());
+	}
+	for (let i = 0; i < dieToSum.length; i++) {
+		dieTotal += dieToSum[i];
+	}
+}
 
-// dieTotal = 0;
-// for (let i = 0; i < dieToSum.length; i++) {
-// 	dieTotal += dieToSum[i];
-// }
-console.log(surgePick, surgeArray[surgePick]);
+console.log(surgePick, surgeArray[surgePick].replace(dieReplace, dieTotal));
